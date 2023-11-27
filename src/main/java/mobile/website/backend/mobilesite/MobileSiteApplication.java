b500e7a59daf612ec0b93f813462bd960feb645c
firehas123 <mr.hassanch@gmail.com>
2023-11-27 20:31:50 +0500

    basic done

---
+++ src/main/java/mobile/website/backend/mobilesite/MobileSiteApplication.java
@@ -0,0 +1,13 @@
+package mobile.website.backend.mobilesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mobile/website/backend/mobilesite/MobileSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package mobile.website.backend.mobilesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
